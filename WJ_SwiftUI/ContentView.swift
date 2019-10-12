7a14c44f11db2f836eab43ba5ed513a4ce3bbbd9
jieyue <jieyue@jieyuedeMacBook-Pro.local>
2019-10-12 15:17:16 +0800

    init

---
+++ WJ_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WJ_SwiftUI
+//
+//  Created by jieyue on 2019/10/12.
+//  Copyright © 2019 wjq. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
